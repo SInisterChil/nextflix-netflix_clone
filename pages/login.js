1a08e70faaca4532294f51fe9db64a5f68a3e377
SHUBHAM KHAIRE <74615743+SInisterChil@users.noreply.github.com>
2023-12-11 16:15:34 +0530

    login update

--- pages/login.js
+++ pages/login.js
@@ -81,16 +81,18 @@ const Login = () => {
 
       <header className={styles.header}>
         <div className={styles.headerWrapper}>
-          <a className={styles.logoLink} href="/">
-            <div className={styles.logoWrapper}>
-              <Image
-                src="/static/netflix.svg"
-                alt="Netflix logo"
-                width="128px"
-                height="34px"
-              />
-            </div>
-          </a>
+          <Link className={styles.logoLink} href="/">
+  <a>
+    <div className={styles.logoWrapper}>
+      <Image
+        src="/static/netflix.svg"
+        alt="Netflix logo"
+        width="128px"
+        height="34px"
+      />
+    </div>
+  </a>
+</Link>
         </div>
       </header>
 
